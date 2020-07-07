c64a754d8b5f2ca83b62e09a55fb686a454db435
chandurajub <chandu.raju.abc@gmail.com>
2020-07-08 00:52:53 +0530

    message

---
+++ VPC1/route.tf
@@ -0,0 +1,18 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = data.aws_availability_zones.azs.names
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
