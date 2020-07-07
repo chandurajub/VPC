b8bb50744cd2a8f60598a82d33382c730de12b58
chandurajub <chandu.raju.abc@gmail.com>
2020-07-07 18:20:01 +0530

    message

--- VPC1/subnet.tf
+++ VPC1/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "main" {
   count = length(data.aws_availability_zones.azs.names)
-  vpc_id     = "${aws_vpc.vpc.id}"
+  vpc_id     = aws_vpc.vpc.id
   availability_zone = data.aws_availability_zones.azs.names[count.index]
   cidr_block = cidrsubnet(var.cidr,8,count.index)
 
