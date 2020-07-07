49d95ddce73c808d0cc84a17737ecb15788b5aef
chandurajub <chandu.raju.abc@gmail.com>
2020-07-07 23:02:29 +0530

    message

--- VPC1/subnet.tf
+++ VPC1/subnet.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "public" {
   cidr_block = cidrsubnet(var.cidr,8,count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = "${var.tags}-subnet-${count.index}"
+    Name = "public-${var.tags}-subnet-${count.index}"
   }
 }
 
@@ -16,6 +16,6 @@ resource "aws_subnet" "private" {
   cidr_block = cidrsubnet(var.cidr,8,length(data.aws_availability_zones.azs.names)+count.index+1)
 
   tags = {
-    Name = "${var.tags}-subnet-${count.index}"
+    Name = "private-${var.tags}-subnet-${count.index}"
   }
 }
