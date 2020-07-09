323a0e7cc2ac245620a031e1e1f64b5f78a7cc63
chandurajub <chandu.raju.abc@gmail.com>
2020-07-09 18:33:33 +0530

    message

---
+++ ec2/Makefile.mk
@@ -0,0 +1,23 @@
+
+.ONESHELL:
+.SHELL := /usr/bin/bash
+.PHONY: prod_apply nonprod_apply prod_destroy nonprod_destroy
+LOG=/tmp/terraform-make.log
+
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[35m%-10s\033[0m %s\n", $$1, $$2}'
+
+prod_apply: ## Apply Module in Prod Environement
+	@terraform init
+	@terraform apply -auto-approve -var-file=prodcidr.tfvars
+
+nonprod_apply: ## Apply Module in NonProd Environement
+	@terraform init
+	@terraform apply -auto-approve -var-file=devcidr.tfvars
+prod_destroy: ## Apply Module in NonProd Environement
+	@terraform init
+	@terraform destroy -auto-approve -var-file=prodcidr.tfvars
+nonprod_destroy: ## Apply Module in NonProd Environement
+	@terraform init
+	@terraform destroy -auto-approve -var-file=devcidr.tfvars
+
