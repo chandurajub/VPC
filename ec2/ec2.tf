1fb1cb075cdcb7009a2d2c8c99836e1942872603
chandurajub <chandu.raju.abc@gmail.com>
2020-07-09 19:04:18 +0530

    message

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "ec2" {
   ami = "ami-0c84a3e93390c29bc"
   instance_type = "t2.micro"
   subnet_id = element(local.private_sub,0)
-  vpc_security_group_ids = [aws_security_group.allow_ssh]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 }
