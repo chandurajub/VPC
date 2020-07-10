53e6c0db8ec73da95a7498ea4d0adc6cdf258287
chandurajub <chandu.raju.abc@gmail.com>
2020-07-10 15:51:17 +0530

    message

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2" {
 }
 resource "aws_instance" "ec21" {
   ami = "ami-0c84a3e93390c29bc"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   subnet_id = element(local.public_sub,0)
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 }
--- ec2/sg.tf
+++ ec2/sg.tf
@@ -22,3 +22,27 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
+resource "aws_security_group" "allow_all" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = "${local.vpc_id}"
+
+  ingress {
+    description = " from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
