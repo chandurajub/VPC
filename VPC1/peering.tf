a2bae61fb0073a9e604fcd3333d73c0e98c14055
chandurajub <chandu.raju.abc@gmail.com>
2020-07-10 18:04:07 +0530

    message

--- VPC1/peering.tf
+++ VPC1/peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = "${var.manage_vpc}"
-  vpc_id        = "${aws_vpc.vpc}"
+  vpc_id        = "${aws_vpc.vpc.id}"
   auto_accept   = true
 
   tags = {
